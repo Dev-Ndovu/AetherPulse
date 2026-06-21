b680dcdf1e2d6b3ce0e464e33912c53cddc0b1dd
Dev-Ndovu <nurusoundchapel@gmail.com>
2026-06-21 15:48:38 +0300

    Create Color.kt

---
+++ app/src/main/java/com/aetherpulse/player/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.aetherpulse.player.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cyberpunk / Sci-Fi Audio Deck Palette
+val ObsidianBlack = Color(0xFF0A0B10)
+val DeepSpaceGrey = Color(0xFF12141C)
+val NeonCyan = Color(0xFF00F0FF)
+val PulseViolet = Color(0xFF9D00FF)
+val ElectricGreen = Color(0xFF39FF14)
+
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF8F94A6)
+val GlassSurface = Color(0x1AFFFFFF) // Translucent glassmorphic layer
